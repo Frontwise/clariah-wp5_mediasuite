0e215d3f5c11fc25c8fad77a61841036bc34f958
Jaap Blom <jblom@beeldengeluid.nl>
2017-12-04 12:55:30 +0100

    working towards moving the annotation api to the server as well

--- src/components/workspace/Workspace.py
+++ src/components/workspace/Workspace.py
@@ -72,3 +72,63 @@ class Workspace():
 			return {'error' : 'Internal server error (route to API not available?)'}, resp.status_code
 
 
+	"""<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+	<><><><><><><><><><> ANNOTATION API REQUESTS <><><><><><><><><>
+	<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>"""
+
+	def processAnnotationAPIRequest(self, clientId, token, method, userId, data=None, annotationId=None):
+		if method == 'DELETE':
+			return self.__deleteAnnotation(clientId, token, userId, annotationId)
+		elif method in ['PUT', 'POST']:
+			return self.__saveAnnotation(clientId, token, userId, data, annotationId)
+		elif method == 'GET':
+			if annotationId:
+				return self.__getAnnotation(clientId, token, userId, annotationId)
+			else:
+				return self.__listAnnotations(clientId, token, userId)
+		return {'error' : 'Bad request'}, 400
+
+	def __getAnnotation(self, clientId, token, userId, annotationId):
+		url = '%s/%s/projects/%s?cid=%s&at=%s' % (
+			self.config['ANNOTATION_API'], userId, annotationId, clientId, token
+		)
+		resp = requests.get(url)
+		if resp.status_code == 200:
+			return resp.text
+		return {'error' : resp.text}, resp.status_code
+
+	def __listAnnotations(self, clientId, token, userId):
+		print 'getting projects'
+		url = '%s/%s/projects?cid=%s&at=%s' % (
+			self.config['ANNOTATION_API'], userId, clientId, token
+		)
+		resp = requests.get(url)
+		if resp.status_code == 200:
+			return resp.text
+		return self.__formatAPIErrorResponse(resp)
+
+	def __saveAnnotation(self, clientId, token, userId, annotation, annotationId=None):
+		url = '%s/%s/annotations?cid=%s&at=%s' % (
+			self.config['ANNOTATION_API'], userId, clientId, token
+		)
+		annotation['clientId'] = clientId
+		annotation['token'] = token
+		if annotationId:
+			url += '/%s' % annotationId
+			resp = requests.put(url, data=annotation)
+		else:
+			resp = requests.post(url, data=annotation)
+		if resp.status_code == 200:
+			return resp.text
+		return self.__formatAPIErrorResponse(resp)
+
+	def __deleteAnnotation(self, clientId, token, userId, annotationId):
+		url = '%s/%s/projects/%s?cid=%s&at=%s' % (
+			self.config['ANNOTATION_API'], userId, annotationId, clientId, token
+		)
+		resp = requests.delete(url)
+		if resp.status_code == 200:
+			return resp.text
+		return self.__formatAPIErrorResponse(resp)
+
+
